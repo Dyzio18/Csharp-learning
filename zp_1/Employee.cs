8b7e275be7e05f4b94d8dd09266aad7e68f86fda
dyzio18 <dyzio18@gmail.com>
2018-10-01 18:36:58 +0200

    [zp-1] Write to file, Add constructor for Employee

--- zp_1/Employee.cs
+++ zp_1/Employee.cs
@@ -1,21 +1,26 @@
 using System;
 
-public class Employee
-{
+public class Employee {
     public string Name;
     public string Surname { get; set; }
     private DateTime BirthDate { get; set; }
-    private string JobPosition { get; set; }
     private double Salary { get; set; }
+    private int JobPosition { get; set; }
 
+    enum Jobs : int { JuniorDeveloper,  MidDeveloper, SeniorDeveloper};
 
-    public Employee()
-    {
-        Name = "Jan";
-        Surname = "Kowalski";
+    public Employee () {
+        this.Name = "undefined";
+        this.Surname = "undefined";
+    }
+    public Employee (string name, string surname, DateTime birth, int salary, int job) {
+        this.Name = name;
+        this.Surname = surname;
+        this.BirthDate = birth;
+        this.Salary = salary;
+        this.JobPosition = job;
     }
 
-    public void printInfo(){
-        Console.WriteLine($"Imie {this.Name}\nNazwisko: {this.Surname}\nData urodzenia: {this.BirthDate}\nPensja: {this.Salary}\nStanowisko: {this.JobPosition}\n");
-    }
+    public string printInfo () => $"Imie {this.Name}\nNazwisko: {this.Surname}\nData urodzenia: {this.BirthDate}\nPensja: {this.Salary}\nStanowisko: {this.JobPosition}\n";
+
 }
--- zp_1/Program.cs
+++ zp_1/Program.cs
@@ -6,21 +6,43 @@ Podstawy jezyka C#, tworzenie klas, skladnia jezyka, serializacja
 
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
 
 namespace zp_1 {
     class Program {
+
+        
+        /// <summary> Write info about employees to file or console
+        /// <para> Collection </para>
+        /// </summary>
+        static void WriteInfoAboutEmployees (List<Employee> employees) {
+
+#if RELEASE
+            using (StreamWriter sw = new StreamWriter ("EmployeeData.txt")) {
+                foreach (Employee employee in employees) {
+                    sw.WriteLine (employee.printInfo ());
+                }
+            }
+#endif
+
+#if DEBUG
+            foreach (Employee employee in employees) {
+                Console.WriteLine (employee.printInfo ());
+            }
+#endif
+
+        }
+
         static void Main (string[] args) {
 
-            List<Employee> employees = new List<Employee>();
-            employees.Add(new Employee());
-            employees.Add(new Employee());
-            employees.Add(new Employee());
+            List<Employee> employees = new List<Employee> ();
+            employees.Add (new Employee ("Brayan", "Kowalski", new DateTime (1995, 12, 6), 3000, 1));
+            employees.Add (new Employee ("Janek", "Kowalski", new DateTime (1990, 6, 1), 5000, 2));
+            employees.Add (new Employee ("Stefan", "Kowalski", new DateTime (1980, 3, 17), 10000, 3));
 
-
-            foreach (Employee employee in employees){
-                employee.printInfo();
-            }
-
+            Program.WriteInfoAboutEmployees (employees);
         }
     }
 }
